a50b65e825b9dd8a47173a2b889ad51321c472df
iamyajat <myajat21@gmail.com>
2021-06-06 18:42:21 +0530

    Initial Commit

---
+++ SwitchCase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwitchCase
+//
+//  Created by Yajat Malhotra on 06/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwitchCase/SwitchCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchCaseApp.swift
+//  SwitchCase
+//
+//  Created by Yajat Malhotra on 06/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwitchCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
